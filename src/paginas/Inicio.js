5a63b46d62d851ccfe1cb3ef843aa5cc2695b3ce
DrGrodus <ebermd00@gmail.com>
2023-05-08 23:13:18 -0500

    ahora se organiza por ids y se acomoda mejor

--- src/paginas/Inicio.js
+++ src/paginas/Inicio.js
@@ -1,4 +1,5 @@
 import {Button, Card, Col, Row} from "react-bootstrap";
+import {useState} from "react";
 import Tel1 from "../img/TelUno.jpg";
 import Tel2 from "../img/TelDos.jpg"
 import Tel3 from "../img/TelTres.jpg"
@@ -8,67 +9,92 @@ import Tel6 from "../img/TelSeis.jpg"
 import "../Componentes/styles.css"
 
 export default function Inicio() {
+
+    //const [contador, setContador] = useState(0);
+    //const [carrito, setCarrito] = useState[{id: "",}];
+
+    const items = [
+        {
+            id: 1,
+            nombre: "Teléfono 1",
+            precio: 100,
+            desc: "Este es el teléfono 1",
+            img: Tel1
+        },
+        {
+            id: 2,
+            nombre: "Teléfono 2",
+            precio: 150,
+            desc: "Este es el teléfono 2",
+            img: Tel2
+        },
+        {
+            id: 3,
+            nombre: "Teléfono 3",
+            precio: 45,
+            desc: "Este es el teléfono 3",
+            img: Tel3
+        },
+        {
+            id: 4,
+            nombre: "Teléfono 4",
+            precio: 1000,
+            desc: "Este es el teléfono 4",
+            img: Tel4
+        },
+        {
+            id: 5,
+            nombre: "Teléfono 5",
+            precio: 120,
+            desc: "Este es el teléfono 5",
+            img: Tel5
+        },
+        {
+            id: 6,
+            nombre: "Teléfono 6",
+            precio: 242,
+            desc: "Este es el teléfono 6",
+            img: Tel6
+        }
+    ];
+    const listItems = items.map(producto =>
+        <Col>
+            <Card key={producto.id}>
+                <Card.Img
+                    variant="top"
+                    src={producto.img}
+                    bsPrefix="fotosInicio"
+                />
+                <Card.Body>
+                    <Card.Title>{producto.nombre}</Card.Title>
+                    <Card.Text>{producto.desc}</Card.Text>
+                    <Card.Text>${producto.precio}</Card.Text>
+                </Card.Body>
+                <div className="botonesInicio">
+                    <Button variant="primary">Ver más</Button>
+                    <Button variant="secondary">Agregar al carrito</Button>
+                </div>
+            </Card>
+        </Col>
+    );
+
+
+    const update = (e) => {
+        const value = e.target.value;
+        //e.preventDefault();
+        console.log("incremento");
+    };
+
+
     return (
         <>
             <h1>Pagina Principal</h1>
             <div className="tienda" id="tienda">
-                <Row xs={2} md={3} lg={3} style={{padding: "10%"}}>
-                    <Col>
-                        <Card>
-                            <Card.Img
-                                variant="top"
-                                src={Tel1}
-                                bsPrefix="fotosInicio"
-                                alt="Primer producto tarjeta"/>
-                            <Card.Body>
-                                <Card.Title>Titulo de tarjeta</Card.Title>
-                                <Card.Text>Texto de ejemplo</Card.Text>
-                                <div className="botonesInicio">
-                                    <Button variant="primary">Ver más</Button>
-                                    <Button variant="secondary">Agregar al carrito</Button>
-                                </div>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-
-                    <Col>
-                        <Card>
-                            <Card.Img
-                                variant="top"
-                                src={Tel2}
-                                bsPrefix="fotosInicio"
-                                alt="Segundo producto tarjeta"/>
-                            <Card.Body>
-                                <Card.Title>Titulo de tarjeta</Card.Title>
-                                <Card.Text>Texto de ejemplo</Card.Text>
-                                <div className="botonesInicio">
-                                    <Button variant="primary">Ver más</Button>
-                                    <Button variant="secondary">Agregar al carrito</Button>
-                                </div>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-
-                    <Col>
-                        <Card>
-                            <Card.Img
-                                variant="top"
-                                src={Tel3}
-                                bsPrefix="fotosInicio"
-                                alt="Tercer producto tarjeta"/>
-                            <Card.Body>
-                                <Card.Title>Titulo de tarjeta</Card.Title>
-                                <Card.Text>Texto de ejemplo</Card.Text>
-                                <div className="botonesInicio">
-                                    <Button variant="primary">Ver más</Button>
-                                    <Button variant="secondary">Agregar al carrito</Button>
-                                </div>
-                            </Card.Body>
-                        </Card>
-                    </Col>
+                <Row xs={2} md={3} lg={4}>
+                    {listItems}
                 </Row>
             </div>
-
         </>
     );
+
 }
